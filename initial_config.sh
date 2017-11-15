a736f023bfec2b2f04d304695c20b13c7e247d9a
SebastianHolzer <SebastianHolzer@users.noreply.github.com>
2017-11-15 21:03:26 +0100

    added current dev version of raspi-config

--- initial_config.sh
+++ initial_config.sh
@@ -5,18 +5,22 @@ if [ $(id -u) -ne 0 ]; then
   exit 1
 fi
 
+# once latest raspi-config is released, that should be used
+RASPI_CONFIG="$(dirname "$(readlink -f "$0")")/raspi_config nonint"
 
-. /usr/bin/raspi-config nonint
-
-do_hostname rpi-$(cut -d: -f4- < /sys/class/net/eth0/address | tr -d :)
-do_wifi_country BE
-do_change_timezone "Europe/Brussels"
-do_boot_behaviour B1 #B1=console, B3=Desktop, n+1=autologon
-do_boot_wait 1
-do_boot_splash 1
+echo Updating system settings...
+$RASPI_CONFIG do_hostname rpi-$(cut -d: -f4- < /sys/class/net/eth0/address | tr -d :)
+$RASPI_CONFIG do_wifi_country 'BE'
+$RASPI_CONFIG do_change_timezone 'Europe/Brussels'
+$RASPI_CONFIG do_configure_keyboard 'be'
+$RASPI_CONFIG do_boot_behaviour B1 #B1=console, B3=Desktop, n+1=autologon
+$RASPI_CONFIG do_boot_wait 1
+$RASPI_CONFIG do_boot_splash 1
 
 # interfaces
-do_ssh 0
-do_spi 0
-do_i2c 0
-do_onewire 0
+$RASPI_CONFIG do_ssh 0
+$RASPI_CONFIG do_spi 0
+$RASPI_CONFIG do_i2c 0
+$RASPI_CONFIG do_onewire 0
+
+#TODO: add nmct user & disable pi
